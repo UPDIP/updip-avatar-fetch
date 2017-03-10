f7b466b6df08cdaf1b3cf928f4a3c7af7558865f
Kai Wood <kwood@kwd.io>
2017-03-10 12:02:57 +0100

    Use interval

--- index.js
+++ index.js
@@ -4,22 +4,29 @@ const fetch = require('node-fetch')
 
 const targetDir = process.env.UPDIP_AVATAR_FETCH_DIR || './images/'
 
-fetcher.getUsersFromUpdip().then(users => {
-  return fetcher.getLinksToProfileImages(users);
-}).then(users => {
-  console.log(users);
+function run() {
+  fetcher.getUsersFromUpdip().then(users => {
+    return fetcher.getLinksToProfileImages(users);
+  }).then(users => {
+    console.log(users);
 
-  users.forEach(user => {
-    fetch(user.avatar).then(res => {
-      let dest = fs.createWriteStream(`${targetDir}${user.user}.png`)
-      res.body.pipe(dest)
-    }).then(() => {
-      console.log(new Date(), user.user, 'fetched successfully');
-    }, err => {
-      console.log(err);
+    users.forEach(user => {
+      fetch(user.avatar).then(res => {
+        let dest = fs.createWriteStream(`${targetDir}${user.user}.png`)
+        res.body.pipe(dest)
+      }).then(() => {
+        console.log(new Date(), user.user, 'fetched successfully');
+      }, err => {
+        console.log(err);
+      })
     })
-  })
 
-}).catch(err => {
-  console.log(err);
-})
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+run();
+setInterval(() => {
+  run()
+}, 3600000)
